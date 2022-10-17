7fcfba7b6cd648cd0a15c85a207d3c7d2aa89a79
ical10 <mochhusnir@gmail.com>
2022-10-17 09:36:45 +0700

    chore: added ConfigModule for env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
